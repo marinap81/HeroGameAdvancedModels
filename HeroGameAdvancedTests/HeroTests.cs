1db77a9cfc5819e9dc408e5e0061020087316284
Marina Pajvancic <103340660@student.swin.edu.au>
2021-11-05 12:33:58 +1100

    herotest completed

--- HeroGameAdvancedTests/HeroTests.cs
+++ HeroGameAdvancedTests/HeroTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 using HeroGameAdvancedLib;
 
@@ -6,10 +7,30 @@ namespace HeroGameAdvancedTests
 {
     public class HeroTests
     {
-        [Fact]
-        public void Test1()
-        {
 
-        }
+        Hero Hero1;
+        public HeroTests(){}
+        /*Hero is able to attack a given number of times.  Each time a hero attacks subtract 1 from Uses
+        // timesToAttack is greater that the number of Uses should return -1 and not subtract from Uses. */
+
+        [Theory]
+        [InlineData(3,1,2)]
+        [InlineData(2,1,1)] 
+        [InlineData(1,2,-1)] //timestoAttack is > than uses
+        public void AttackTest(int fromUsesLeft, int subtract, int expected)
+        {
+            Dice d = new Dice(1,6);
+
+            // Arrange - set the test up
+            Hero h = new Hero(1,"testHero",d,fromUsesLeft);
+
+            // Act -  call the method to be tested
+            h.Attack(subtract);
+            int result = h.Uses;
+
+            // Assert - check is the answer is correct
+            Assert.Equal(expected, result);
+        }   
+
     }
 }
--- HeroGameAdvancedTests/VillainTests.cs
+++ HeroGameAdvancedTests/VillainTests.cs
@@ -1,11 +1,17 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 using HeroGameAdvancedLib;
+
 
 namespace HeroGameAdvancedTests
 {
     public class VillainTests
     {
+        Villain villain1;
+
+        public VillainTests(){}
+
         [Fact]
         public void Test1()
         {
